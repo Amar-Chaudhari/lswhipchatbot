c74ff278bc44e06587614f43e678684af4fe0aca
Amar-Chaudhari <contact@amarchaudhari.me>
2015-03-15 16:12:30 +0900

    Initial Commit

---
+++ config.py
@@ -0,0 +1,2 @@
+__author__ = 'amarchaudhari'
+lsw_api_key=""
---
+++ lswbot.py
@@ -0,0 +1,22 @@
+__author__ = 'amarchaudhari'
+import config
+import httplib
+# Request: LeaseWeb API (https://api.leaseweb.com/v1/bareMetals)
+
+connection = httplib.HTTPConnection('api.leaseweb.com', 443, timeout = 30)
+
+# Headers
+
+headers = {"X-Lsw-Auth": '%s'} % config.lsw_api_key
+
+# Send synchronously
+
+connection.request('GET', '/v1/bareMetals', None, headers)
+try:
+	response = connection.getresponse()
+	content = response.read()
+	# Success
+	print('Response status ' + str(response.status))
+except httplib.HTTPException, e:
+	# Exception
+	print('Exception during request')
