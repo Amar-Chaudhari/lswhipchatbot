3045551b1ce3b9fb16ed70330a3c02951ec67352
Amar-Chaudhari <contact@amarchaudhari.me>
2015-03-15 18:28:07 +0900

    added new functions

--- lswbot.py
+++ lswbot.py
@@ -5,9 +5,31 @@ from will.plugin import WillPlugin
 from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
 
 
-class BonjourPlugin(WillPlugin):
+class LswBot(WillPlugin):
 
     @respond_to("switchport status (?P<server_id>.*)")
-    def say_bonjour_will(self, message,server_id):
-        self.reply(message, "server online "+str(server_id))
+    def say_switchport_status(self, message,server_id):
+        if server_id:
+            # Request: LeaseWeb API (https://api.leaseweb.com/v1/bareMetals)
+            lsw_key = config.lsw_api_key
+            # Send synchronously
+            try:
+                r =requests.get("https://api.leaseweb.com/v1/bareMetals",headers={"Accept": "application/json","X-Lsw-Auth": lsw_key })
+                # Success
+                print('Response status ' + str(r.status_code))
+                data = r.json()
+                for server in data['bareMetals']:
+                    if server['bareMetal']['serverName'] == server_id:
+                        status=server
+                        break
+                if status:
+                    self.reply(message, status)
+                else:
+                    self.reply(message, "Status could not be determined")
+            except requests.exceptions.Timeout, e:
+            #Exception
+                self.reply(message, "Request timed out , try again !")
+
+        else:
+            self.reply(message, "No Server ID")
 
