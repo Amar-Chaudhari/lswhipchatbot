5b98b43f567e29d2c9a54355c00797a6ade7e029
Amar-Chaudhari <contact@amarchaudhari.me>
2015-03-15 17:16:59 +0900

    request header change

--- lswbot.py
+++ lswbot.py
@@ -4,13 +4,13 @@ import requests
 
 # Request: LeaseWeb API (https://api.leaseweb.com/v1/bareMetals)
 lsw_key = config.lsw_api_key
-headers = {"Accept": "application/json","X-Lsw-Auth": lsw_key }
+
 
 # Headers
 
 # Send synchronously
 try:
-    r =requests.get("https://api.leaseweb.com/v1/bareMetals",headers)
+    r =requests.get("https://api.leaseweb.com/v1/bareMetals",headers={"Accept": "application/json","X-Lsw-Auth": lsw_key })
     # Success
     print('Response status ' + str(r.status_code))
     data = r.json()
