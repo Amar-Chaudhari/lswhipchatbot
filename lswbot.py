54575693ecfb4a34cf7bcf342236ec87effe6e09
Amar-Chaudhari <contact@amarchaudhari.me>
2015-03-15 16:42:36 +0900

    removed config parser

--- lswbot.py
+++ lswbot.py
@@ -1,16 +1,13 @@
 __author__ = 'amarchaudhari'
 import config
 import httplib
-import ConfigParser
 # Request: LeaseWeb API (https://api.leaseweb.com/v1/bareMetals)
 
 connection = httplib.HTTPConnection('api.leaseweb.com', 443, timeout = 30)
 
 # Headers
-
-config = ConfigParser.ConfigObj()
-
-headers = {"X-Lsw-Auth": '%s' } % str(config['lsw_api_key'])
+print config.lsw_api_key
+headers = {"X-Lsw-Auth": '%s' } % str(config.lsw_api_key)
 
 # Send synchronously
 
