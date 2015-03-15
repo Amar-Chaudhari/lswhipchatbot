9a23cf319e116277874b0b03afac6604dd85a2c5
Amar-Chaudhari <contact@amarchaudhari.me>
2015-03-15 18:04:21 +0900

    trying

--- lswbot.py
+++ lswbot.py
@@ -7,7 +7,7 @@ from will.decorators import respond_to, periodic, hear, randomly, route, rendere
 
 class BonjourPlugin(WillPlugin):
 
-    @respond_to("switchport status (?P<server_id>.*)")
+    @respond_to("switchport status (?P<server_id>.(BWND*)")
     def say_bonjour_will(self, message,server_id):
         self.reply(message, "server online"+str(server_id))
 
